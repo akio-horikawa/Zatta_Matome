ca62875e2dca8b6ce5f8eccd3a340cf8b67a4a2d
akio-horikawa <ruou1535@gmail.com>
2023-12-06 10:59:36 +0900

    いつものやつ

---
+++ JS/test_nandemo copy.js
@@ -0,0 +1,10 @@
+{ <script>
+  var aTags = document.querySelectorAll(".public-DraftStyleDefault-block a");
+  aTags.forEach(function(aTag){
+    aTag.addEventListener("click", function(event){
+      event.preventDefault();
+      //デプロイ対策でコメント追加
+      console.log("リンクがクリックされました");
+    })
+  });
+</script> }
