46e60441e41f9ee42b76a15cd8bc4261e8627aba
akio-horikawa <ruou1535@gmail.com>
2023-12-06 15:42:23 +0900

    react弄り

---
+++ JS/app-matome/react-test/src/ApplePie.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header class="header">
+        <h2>Apple pie!</h2>
+      </header>
+
+      
+
+    </div>
+  );
+}
+
+export default App;
