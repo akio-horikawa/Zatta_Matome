83fc8ad07b497b2956290f7b09b08f22b87bf760
akio-horikawa <ruou1535@gmail.com>
2023-12-12 15:18:15 +0900

    最小環境

--- JS/draft-test-app/src/App.js
+++ JS/draft-test-app/src/App.js
@@ -8,22 +8,13 @@ const initData = convertFromRaw({
  entityMap: {},
  blocks: [
    {
-     key: "xxxxx",
-     text: "ここだよ！",
+     key: "apple",
+     text: "Apple pie!",
      type: "unstyled",
      depth: 0,
      entityRanges: [],
      inlineStyleRanges: [],
      data: {},
-   },
-   {
-    key: "deleteMe",
-    text: "すぐにけせ",
-    type: "header-one",
-    depth: 0,
-    entityRanges: [],
-    inlineStyleRanges: [],
-    data: {},
    },
  ],
 });
@@ -32,69 +23,10 @@ const initState = EditorState.createWithContent(
  initData,
 );
 
-function removeBlockKey(editorState, blockKey){
-  const contentState = editorState.getCurrentContent();
-  const block = contentState.getBlockForKey(blockKey);
-  const blockRange = new SelectionState({
-    anchorKey: blockKey,
-    anchorOffset: 0,
-    focusKey: blockKey,
-    focusOffset: block.getLength(),
-  });
-  const newContentState = Modifier.removeRange(contentState, blockRange, 'backward');
-  const newEditorState = EditorState.push(editorState, newContentState, 'remove-range');
-
-  return newEditorState;
-}
-
 function App() {
  const [editorState, setEditorState] = useState(
    initState
  );
-
- const keyBindingFn = (e) => {
-   if (e.key === "Enter") {
-     alert("アップルパイ！")
-     return "disabled"
-   }
-   if (e.key === "Backspace") {
-     alert("油揚げ！")
-     const newEditorState = removeBlockKey(editorState, "deleteMe");
-    if (newEditorState !== editorState) {
-      setEditorState(newEditorState);
-      return "handled";
-    }
-   }
-   return getDefaultKeyBinding(e)
- }
-
- const LinkBlock = ({ block, blockProps }) => {
-  const { readOnly } = blockProps;
-  const link = block.getData().get('link');
-  return (
-   <div contentEditable={!readOnly}>
-     <a href={link} onClick={(event) => {
-      event.preventDefault();
-      console.log("Link Clicked!" , link);
-    }}>
-      {block.getText()}
-     </a>
-   </div>
-  );
- }
-
- const myBlockRenderer = (block) => {
-  if (block.getType() === "unstyled") {
-   return {
-     component: LinkBlock,
-     props: {
-       readOnly: true,
-     },
-   }
-  }
-  return null
- }
-
 
  return (
   <div className="App">
@@ -104,8 +36,6 @@ function App() {
     <Editor
       editorState={editorState}
       onEditorStateChange={setEditorState}
-      keyBindingFn={keyBindingFn}
-      blockRendererFn={myBlockRenderer}
       localization={{
         locale: "ja",
       }}
@@ -113,7 +43,7 @@ function App() {
   </div>
 
 
- )
+ );
 }
 
 export default App;
