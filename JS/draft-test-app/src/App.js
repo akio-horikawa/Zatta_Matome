387538dd30cf087f35d86219a6adce084f5d69e2
akio-horikawa <ruou1535@gmail.com>
2023-12-20 17:24:27 +0900

    react

--- JS/draft-test-app/src/App.js
+++ JS/draft-test-app/src/App.js
@@ -1,48 +1,14 @@
 import React, { useState } from 'react';
-import { EditorState, convertFromRaw} from 'draft-js';
-import { Editor } from 'react-draft-wysiwyg';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import './App.css';
 
-const initData = convertFromRaw({
- entityMap: {},
- blocks: [
-   {
-     key: "apple",
-     text: "Apple pie!",
-     type: "unstyled",
-     depth: 0,
-     entityRanges: [],
-     inlineStyleRanges: [],
-     data: {},
-   },
- ],
-});
-
-const initState = EditorState.createWithContent(
- initData,
-);
 
 function App() {
- const [editorState, setEditorState] = useState(
-   initState
- );
-
  return (
   <div className="App">
-    <header className="App-header">
-      Rich Text Editor
+    <header>
+      
     </header>
-    <Editor
-      editorState={editorState}
-      onEditorStateChange={setEditorState}
-      localization={{
-        locale: "ja",
-      }}
-    />
   </div>
-
-
  );
 }
 
