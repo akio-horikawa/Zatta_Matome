832c5d99880e783ab62fd7ec1af740841c3ae49e
akio-horikawa <ruou1535@gmail.com>
2023-12-26 14:30:24 +0900

    chakra-ui hover tooltip etc

--- JS/draft-test-app/src/App.js
+++ JS/draft-test-app/src/App.js
@@ -4,16 +4,20 @@ import './App.css';
 import {Box, Button, Tooltip, extendTheme, ChakraProvider} from "@chakra-ui/react";
 
 const customTheme = extendTheme({
+  fonts: {
+    heading: "游ゴシック体, Yu Gothic, YuGothic, ヒラギノ角ゴシック Pro, Hiragino Kaku Gothic Pro, メイリオ, Meiryo, Osaka, ＭＳ Ｐゴシック, MS PGothic, sans-serif",
+    body: "游ゴシック体, Yu Gothic, YuGothic, ヒラギノ角ゴシック Pro, Hiragino Kaku Gothic Pro, メイリオ, Meiryo, Osaka, ＭＳ Ｐゴシック, MS PGothic, sans-serif",
+    },
   components: {
     Tooltip: {
-      baseStyle: {
-        fontSize: "0.1em",
-        fontWeight: "bold",
-        backgroundColor: "blue.400",
-        padding: "2px",
-        borderRadius: "2px",
-        boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.1)",
-      },
+        baseStyle: {
+          fontSize: "0.1em",
+          fontWeight: "bold",
+          backgroundColor: "blue.400",
+          padding: "2px",
+          borderRadius: "2px",
+          boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.1)",
+        },
     },
   },
 })
@@ -36,11 +40,12 @@ function App() {
  return (
     <div>
       <header className="App">
-        TextTest
-        <Box display="inline-block">
+        石炭
+        <Box display="inline-block" fontFamily="Yu Gothic, sans-serif">
           <ChakraProvider theme={customTheme}>
             <Tooltip label = "test" placement="left" fontSize="md">
               <div>
+                石炭
                 <ImTwitter
                   style={{marginLeft : "10px"}}
                   onClick={ handleClick }
@@ -62,14 +67,20 @@ function App() {
       </header>
 
       <body>
-        <Box display="inline-block" style={{ margin: "10rem" }}>
+        石炭
           <ChakraProvider theme={Xtheme} >
+        <Box display="inline-block" style={{ margin: "10rem" }} fontFamily="Yu Gothic, sans-serif">
             <Tooltip label="X" fontSize="md" placement="left">
               <Button colorScheme="blue"><ImTwitter marginLeft="5px"/>Twitter</Button>
             </Tooltip>
-          </ChakraProvider>
+            石炭
         </Box>
+          </ChakraProvider>
+        石炭
       </body>
+      <hooter>
+        石炭
+      </hooter>
     </div>
  );
 }
