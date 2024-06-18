65be48d8dc1dff1d1de25ac09b5717e8ed4eeaaf
akio-horikawa <ruou1535@gmail.com>
2024-06-18 16:35:35 +0900

    push

--- JS/test_nandemo copy.js
+++
@@ -1,9 +0,0 @@
-{
-  document.querySelectorAll(".public-DraftStyleDefault-block a")
-    .forEach(function(aTag){
-      aTag.addEventListener("click", function(event){
-        event.preventDefault();
-        console.log("リンクがクリックされました");
-      })
-    })
-}
